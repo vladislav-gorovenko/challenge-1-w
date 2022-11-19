800ce1780e7f6c37cb1a928b29bca0a901139ec1
martinideniam <martinideniam@gmail.com>
2022-11-19 19:45:06 +0000

    mobile friendly

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("hello")
+let burgerEl = document.querySelector(".burger") 
+let hiddenMenuEl = document.querySelector(".hidden-menu")
+
+burgerEl.addEventListener("click", () => {
+    hiddenMenuEl.classList.toggle("move-in")
+    hiddenMenuEl.classList.toggle("shadow")
+    burgerEl.classList.toggle("burger-pressed")
+})
